a32573f7911971bf1a4386d02a3e3d081f62532b
Jon <jon@stencyl.com>
2012-09-14 01:15:04 +0000

    [3.0] Purchases extension

--- Purchases.hx
+++ Purchases.hx
@@ -25,52 +25,54 @@ class Purchases
 	private static function registerHandle()
 	{
 		#if cpp
-			set_event_handle(notifyListeners);
+		set_event_handle(notifyListeners);
 		#end
 	}
 	
 	private static function notifyListeners(inEvent:Dynamic)
 	{
 		#if cpp
-			var type:Int = Std.int(Reflect.field(inEvent, "type"));
-			var code:Int = Std.int(Reflect.field(inEvent, "code"));
-			var value:Int = Std.int(Reflect.field(inEvent, "value"));
-			var data:String = Std.string(Reflect.field(inEvent, "data"));
-			var event:PurchaseEvent;
+		var type:Int = Std.int(Reflect.field(inEvent, "type"));
+		var code:Int = Std.int(Reflect.field(inEvent, "code"));
+		var value:Int = Std.int(Reflect.field(inEvent, "value"));
+		var data:String = Std.string(Reflect.field(inEvent, "data"));
+		var event:PurchaseEvent;
+		
+		switch(type)
+		{
+			case 0:  event = new PurchaseEvent(PurchaseEvent.UNKNOWN, code, value, data);
+			case 1:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_SUCCESS, code, value, data);
+			case 2:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_FAIL, code, value, data);
+			case 3:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_CANCEL, code, value, data);
+			default: event = new PurchaseEvent(PurchaseEvent.UNKNOWN, code, value, data);
+		}
+		
+		//Consumable
+		if(type == 1)
+		{
+			var productID = data;
 			
-			switch(type)
+			if(hasBought(productID))
 			{
-				case 0:  event = new PurchaseEvent(PurchaseEvent.UNKNOWN, code, value, data);
-				case 1:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_SUCCESS, code, value, data);
-				case 2:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_FAIL, code, value, data);
-				case 3:  event = new PurchaseEvent(PurchaseEvent.IN_APP_PURCHASE_CANCEL, code, value, data);
-				default: event = new PurchaseEvent(PurchaseEvent.UNKNOWN, code, value, data);
+				items.set(productID, Purchases.items.get(productID) + 1);
 			}
 			
-			if(type == 1)
+			else
 			{
-				var productID = data;
-				
-				if(hasBought(productID))
-				{
-					items.set(productID, Purchases.items.get(productID) + 1);
-				}
-				
-				else
-				{
-					items.set(productID, 1);
-				}
-			
-				save();
+				items.set(productID, 1);
 			}
-			
-			for(i in 0...listeners.length)
+		
+			save();
+		}
+		
+		//Notify Listeners
+		for(i in 0...listeners.length)
+		{
+			if(listeners[i] != null)
 			{
-				if(listeners[i] != null)
-				{
-					listeners[i](event);
-				}
-			}	
+				listeners[i](event);
+			}
+		}	
 		#end
 	}
 	
@@ -92,21 +94,28 @@ class Purchases
 	public static function initialize():Void 
 	{
 		#if cpp
-			if(!initialized)
-			{
-				registerHandle();
-				load();
-				
-				initialized = true;
-			}
+		if(!initialized)
+		{
+			registerHandle();
+			load();
 			
-			purchases_initialize();
+			initialized = true;
+		}
+		
+		purchases_initialize();
 		#end	
+	}
+	
+	public static function restorePurchases():Void
+	{
+		#if cpp
+		//TODO
+		#end
 	}
 	
 	private static function load()
 	{
-		#if !js
+		#if cpp
 		try 
 		{
 			var data = SharedObject.getLocal("in-app-purchases");
@@ -123,18 +132,17 @@ class Purchases
 		{
 			trace("Error! Failed to load purchases: " + e);
 		}
-		
 		#end
 	}
 	
 	private static function save()
 	{
-		#if !js
+		#if cpp
 		var so = SharedObject.getLocal("in-app-purchases");
 		Reflect.setField(so.data, "data", items);
 		#end
 		
-		#if ( cpp || neko )
+		#if (cpp || neko)
 		var flushStatus:SharedObjectFlushStatus = null;
 		#else
 		var flushStatus:String = null;
@@ -157,6 +165,7 @@ class Purchases
 		    {
 		        case SharedObjectFlushStatus.PENDING:
 		            trace("Requesting Permission to Save Purchases");
+		            
 		        case SharedObjectFlushStatus.FLUSHED:
 		            trace("Saved Purchases");
 		    }
@@ -167,54 +176,74 @@ class Purchases
 	//True if they've bought this before. If consumable, if they have 1 or more of it.
 	public static function hasBought(productID:String)
 	{
+		#if cpp
 		if(items == null)
 		{
 			return false;
 		}
 		
 		return items.exists(productID) && items.get(productID) > 0;
+		#end
+		
+		#if !cpp
+		return false;
+		#end
 	}
 	
 	//Uses up a "consumable" (decrements its count by 1).
 	public static function use(productID:String)
 	{
+		#if cpp
 		if(hasBought(productID))
 		{
 			items.set(productID, items.get(productID) - 1);
 			save();
 		}
+		#end
+	}
+	
+	public static function getQuantity(productID:String):Int
+	{
+		#if cpp
+		if(hasBought(productID))
+		{
+			return items.get(productID);
+		}
+		#end
+		
+		return 0;
 	}
 
 	public static function buy(productID:String):Void 
 	{
 		#if cpp
-			purchases_buy(productID);
+		purchases_buy(productID);
 		#end	
 	}
 	
 	public static function canBuy():Bool 
 	{
 		#if cpp
-			return purchases_canbuy();
+		return purchases_canbuy();
 		#end
 		
 		#if !cpp
-			return false;
+		return false;
 		#end
 	}
 	
 	public static function release():Void 
 	{
 		#if cpp
-			purchases_release();
+		purchases_release();
 		#end
 	}
 
 	#if cpp
-		private static var purchases_initialize = Lib.load("purchases", "purchases_initialize", 0);
-		private static var purchases_buy = Lib.load("purchases", "purchases_buy", 1);
-		private static var purchases_canbuy = Lib.load("purchases", "purchases_canbuy", 0);
-		private static var purchases_release = Lib.load("purchases", "purchases_release", 0);
-		private static var set_event_handle = Lib.load("purchases", "purchases_set_event_handle", 1);
+	private static var purchases_initialize = Lib.load("purchases", "purchases_initialize", 0);
+	private static var purchases_buy = Lib.load("purchases", "purchases_buy", 1);
+	private static var purchases_canbuy = Lib.load("purchases", "purchases_canbuy", 0);
+	private static var purchases_release = Lib.load("purchases", "purchases_release", 0);
+	private static var set_event_handle = Lib.load("purchases", "purchases_set_event_handle", 1);
 	#end
 }
